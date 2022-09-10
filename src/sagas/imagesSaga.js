5f9e2cec151b08f67ac19f85568f7f0aa85a8632
sumeethMacPro <sumeethkanojia@gmail.com>
2022-09-10 21:28:13 +0530

    init commit

---
+++ src/sagas/imagesSaga.js
@@ -0,0 +1,22 @@
+import { put, call, takeEvery, select } from 'redux-saga/effects';
+
+import { setImages, setError } from '../actions';
+import { IMAGES } from '../constants';
+import { fetchImages } from '../api';
+
+export const getPage = state => state.nextPage;
+
+export function* handleImagesLoad() {
+    try {
+        const page = yield select(getPage);
+        const images = yield call(fetchImages, page);
+        console.log("Fetch Image: ", images,page)
+        yield put(setImages(images));
+    } catch (error) {
+        yield put(setError(error.toString()));
+    }
+}
+
+export default function* watchImagesLoad() {
+    yield takeEvery(IMAGES.LOAD, handleImagesLoad);
+}
